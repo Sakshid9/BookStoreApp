27c7e186d7fb0e6457eae6b82f797b48c931345b
Sakshi Dubey <dubeysakshi9292@gmail.com>
2026-04-11 01:53:47 +0530

    Fixing case sensitivity for Freebook import

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Banner from "../components/Banner";
 import Footer from "../components/Footer";
-import Freebook from "../components/freebook";
+import Freebook from "../components/Freebook";
 
 function Home() {
   return (
